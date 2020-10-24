324099e5cb74c98d83dacf5619477011940a9354
Braslyn Rodríguez Ramírez <braslynrrr999@gmail.com>
2020-10-24 15:09:31 -0600

    Postfijo

--- Compartido/Prolog/arboles.pl
+++ Compartido/Prolog/arboles.pl
@@ -1,15 +1,23 @@
 postfijo(Exp,[Exp]):- atomic(Exp).
 postfijo(Exp,L):- Exp=..[Op,Left,Rigth],
 						member(Op,['-','+','/','*']),
-						postfijo(Left,L1),
-						postfijo(Rigth,L2),
+						postfijo(Rigth,L1),
+						postfijo(Left,L2),
 						postfijo(Op,L3),
 						append(L2,L1,L4),
 						append(L4,L3,L).
+%Malo				
+post(Exp,L,[Exp|L]):- atomic(Exp).
+postfijo2(Exp,L):- Exp=..[Op,Left,Rigth],
+						member(Op,['-','+','/','*']),
+						post(Rigth,[],L1),
+						post(Left,L1,L2),
+						post(Op,L2,L3),
+						reverse(L3,L).
 						
 eval(X,Mem,V):- atom(X),member([X,V],Mem).
 eval(N,_,N):- number(N).
-eval(T,Mem,R):- T=.. [Op,Left,Rigth],
+eval(T,Mem,R):- T=.. [Op,Left,Rigth],write(Op),
 					member(Op,['-','+','/','*']),
 					eval(Left,Mem,Vleft),
 					eval(Rigth,Mem,VRight),
