ce9717d139947439c4bae0e575bcbda510ff1a29
Braslyn Rodríguez Ramírez <braslynrrr999@gmail.com>
2020-11-11 01:56:59 -0600

    prolog

--- Compartido/Prolog/ejercicios.pl
+++ Compartido/Prolog/ejercicios.pl
@@ -12,6 +12,16 @@ pertenece(X,[_|L]):- pertenece(X,L).
 conc([],Y,Y).
 conc([F|R],Y,[F|Z]):- conc(R,Y,Z).
 ultimo(X,[X]).
-ultimo(X,[F|R]):-ultimo(X,R).
+ultimo(X,[_|R]):-ultimo(X,R).
 penultimo(X,[X,_]).
-penultimo(X,[F|R]):- penultimo(X,R).
+penultimo(X,[_|R]):- penultimo(X,R).
+
+
+
+
+non_adjacent([F,R],X,Y):- append([F],X,R), non_adjacent(R,X,Y).
+
+
+last(L,U):- foldl([X,_,M] >> (M = X), L,_,U).
+
+occurs(L,V,N):- foldl([X,Y,Acc] >> ( X\==V -> Acc is Y ;X==V -> Acc is Y+1 ) , L , 0 , N).%mal pero casi bien
