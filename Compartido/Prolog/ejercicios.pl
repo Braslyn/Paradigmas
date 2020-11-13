61e9335c6850a6fd97ed09a0055a423e79a90211
Braslyn Rodríguez Ramírez <braslynrrr999@gmail.com>
2020-11-12 22:46:20 -0600

    gramatica

--- Compartido/Prolog/ejercicios.pl
+++ Compartido/Prolog/ejercicios.pl
@@ -16,17 +16,11 @@ ultimo(X,[_|R]):-ultimo(X,R).
 penultimo(X,[X,_]).
 penultimo(X,[_|R]):- penultimo(X,R).
 
-
-non_adjacent(L,X,Y):- append(_,[X,_|_],A),append(A,[Y|_],L).
-
-%equi_distant([F|R], N, X, Y):- append([X|_],[_|Y],[F|R]).
-
 last(L,U):- foldl([X,_,M] >> (M = X), L,_,U).
-%last(L,U):- foldl([X,M,M] >> (M = X), L,_,U).
+last2(L,U):- foldl([X,_,X]>>true, L,_,U).
 
 occurs(L,V,N):- foldl({V}/[X,Y,Acc] >> ( X==V -> (Acc is Y+1);(Acc is Y) ) , L , 0 , N).%mal pero casi bien
 
-%reduce(Lambda,L,V0,V):-
 
 reduce(_,[],V0,V0).
 reduce(Lambda,[F|L],V0,V):- call(Lambda,F,V0,Acc),reduce(Lambda,L,Acc,V).
@@ -50,9 +44,44 @@ reduce2(Lambda,L,_,_):-
 	
 reduce2(_,_,_,V):- retract(data(V)).
 
+add(z,N,N).
+add(s(N),M,R):- add(N,s(M),R).
 
-	
-	
+mult(z,N,N).
+mult(s(M),N, R):- add(N,MN,R),mult(M,N,MN).
+
+toNum(z,0).
+toNum(s(N),M):- toNum(N,M0), M is M0+1.
+%toNum(p(N),M):- toNum(N,M0), M is M0-1.
+
+
+simplify(z,_).
+simplify(s(N),R):-simplify(N,s(R)).
+simplify(p(N),R):-simplify(N,p(R)).
+
+
+punto_medio(A,X,L,R):- length(A,_var),Length is _var//2,append(L,[X|R],A), length(L,Length),!.
+
+equi_distant(L, N, X, Y):- append(_,[X|R],L),append(M,[Y|_],R),length(M,N).
+
+non_adjacent(L,X,Y):- append(_,[X,_|_],A),append(A,[Y|_],L).
+
+
+test_listas :- 
+    atomic_list_concat(Tokens, ' ',  '[ 1 , 2 , 3 ]'),
+    lista(List, Tokens, []),
+    List = [1,2,3].
+
+
+lista(X)--> ['['],digits(X),[']'].
+digits([])-->[].
+digits([F|R]) --> digit(F),digits2(R).
+digits2([])-->[].
+digits2([F])--> digit(F). 
+digits2([F|R])-->[','],digit(F),digits2(R).
+digit(N)--> [X],{atom_number(X,N)}.
+
+
 
 
 
